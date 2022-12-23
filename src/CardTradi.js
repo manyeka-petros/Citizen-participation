70b940002e7a1444ee94bf428b62a5357b9cd631
petrosmanyeka <bsc-com-36-19@unima.ac.mw>
2022-12-23 10:36:03 -0800

    added home component

---
+++ src/CardTradi.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardTradi = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CardTradi
---
+++ src/Tradition.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tradition = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Tradition
