c82fcd2403cdf3b1fe7662290d38fdc5ddc00d06
petrosmanyeka <bsc-com-36-19@unima.ac.mw>
2022-12-23 09:58:02 -0800

    i have created navbar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+   <div className='nav'>
+
+    <div className='nav1'>
+    <h3>CITIZEN PARTICIPATION</h3>
+    </div>
+    <div className='nav2'>
+        <a href=''>HOME</a>
+        <a href=''>NDAKATULO</a>
+        <a href=''>TRADITONAL DANCE</a>
+        <a href=''>YOUTH ACTIVITEI</a>
+        <a href=''>YOUTH COMPTI</a>
+        <a href=''>ABOUT</a>
+    </div>
+   </div>
+  )
+}
+
+export default Navbar
