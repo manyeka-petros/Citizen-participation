df287e7ab77b4eb022a12478d1369b796b5e9f0d
petros-manyeka <manyekapetros29@gmail.com>
2022-12-25 06:30:35 -0800

    responsive navbar has been added and improved home component

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import HomeCard from './HomeCard'
+import Video from './Assert/dance.mp4'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <HomeCard
-      titl = 'mapoto'
-      dance = 'manganja'
-      price = '2309'
-      />
-      <HomeCard
-      titl = 'mapoto'
-      dance = 'manganja'
-      price = '2309'
-      />
-      <HomeCard
-      titl = 'mapoto'
-      dance = 'manganja'
-      price = '2309'
-      />
+    <div className='hero'>
+      <video autoPlay  loop muted id='vide'>
+        <source src={Video} type='video/mp4'/>
+
+      </video>
+      <div className='overpl'></div>
+      <div className='cit'>
+        <h1>WELCOME TO UNIMA CITIZEN PARTICIPATION</h1>
+        <h3>Join us for more activities</h3>
+      </div>
     </div>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import { FaBars, FaFacebook, FaInstagram, FaTimes, FaTwitter, FaWhatsapp } from "react-icons/fa";
 
 const Navbar = () => {
+  const [clic,setClic] = useState('')
+  const  handleCli  = ()=> setClic(!clic)
   return (
    <div className='nav'>
 
     <div className='nav1'>
     <h3>CITIZEN PARTICIPATION</h3>
     </div>
-    <div className='nav2'>
-        <a href=''>HOME</a>
-        <a href=''>NDAKATULO</a>
-        <a href='tradit'>TRADITONAL DANCE</a>
-        <a href=''>YOUTH ACTIVITEI</a>
-        <a href=''>YOUTH COMPTI</a>
+    <div className='nav2-menu'>
+        <a href='/'>HOME</a>
+        <a href=''>POEMS</a>
+        <a href='tradit'>TRA DANCES</a>
+        <a href=''>YOUTH-ACTIVITEI</a>
+        <a href=''>COMPTI</a>
         <a href=''>ABOUT</a>
     </div>
+    <div className='icons' onClick={handleCli}>
+      {!clic ?(<FaBars/>) : (<FaTimes/>)}
+         
+    </div>
+
+    <div className={clic ?'mobile-menu active':'mobile-menu'}>
+    <div className='mobile-nav'>
+        <a href='/'>HOME</a>
+        <a href=''>POEMS</a>
+        <a href='tradit'>TRA DANCES</a>
+        <a href=''>YOUTH-ACTIVITEI</a>
+        <a href=''>COMPTI</a>
+        <a href=''>ABOUT</a> 
+        <div className='mobile-icons-button'>
+         <div className='mobile-button'>
+        <button className='btn btn-primary'>Search</button>
+        <br/>
+        <button className='btn btn-primary'>Account</button>
+       </div>
+       <div className='socio-icons'>
+      <FaFacebook/>
+      <FaWhatsapp/>
+      <FaTwitter/>
+      <FaInstagram/>
+
+    </div>
+   </div>
+    </div>
+    </div>
+   
    </div>
   )
 }
